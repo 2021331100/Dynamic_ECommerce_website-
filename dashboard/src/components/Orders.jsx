767921eae894bff7cfe1555edf617f201ce0f94a
2021331100 <138570295+2021331100@users.noreply.github.com>
2025-12-01 08:42:08 -0800

    core-order management logic added to fetch ,filter,Update Orders.jsx

--- dashboard/src/components/Orders.jsx
+++ dashboard/src/components/Orders.jsx
@@ -15,3 +15,164 @@ const Orders = () => {
     "Delivered",
     "Cancelled",
   ];
+const dispatch = useDispatch();
+  const { orders, loading } = useSelector((state) => state.order);
+
+  const [selectedStatus, setSelectedStatus] = useState({});
+  const [filterByStatus, setFilterByStatus] = useState("All");
+  const [previewImage, setPreviewImage] = useState(null);
+  const [deleteConfirm, setDeleteConfirm] = useState({ open: false, id: null });
+
+  useEffect(() => {
+    dispatch(fetchAllOrders());
+  }, [dispatch]);
+
+  const handleStatusChange = (orderId, newStatus) => {
+    setSelectedStatus(newStatus);
+    dispatch(updateOrderStatus({ orderId, status: newStatus }));
+  };
+  const filteredOrders =
+    filterByStatus === "All"
+      ? orders
+      : orders?.filter((order) => order.order_status === filterByStatus);
+
+  const confirmDelete = () => {
+    dispatch(deleteOrder(deleteConfirm.id));
+    setDeleteConfirm({ open: false, id: null });
+  };
+
+  if (loading) return <p className="p-10">Loading Orders...</p>;
+  return (
+    <>
+      <main className="p-[10px] pl-[10px] md:pl-[17rem] w-full">
+        <div className="flex-1 md:p-6">
+          <Header />
+          <h1 className="text-2xl font-bold">All Orders</h1>
+          <p className="text-sm text-gray-600 mb-6">Manage all your orders.</p>
+        </div>
+        {/*CONTENT*/}
+
+        {loading ? (
+          <div className="w-40 h-40 mx-auto border-2 border-white border-t-transparent rounded-full animate-spin"/>
+        ) : (
+          <>
+            {filteredOrders.length === 0 ? (
+              <h3 className="text-2xl font-bold p-6">No orders found.</h3>
+            ) : (
+              <>
+                <div className="flex justify-between items-center p-6">
+                  <select
+                    className="p-2 border rounded shadow-sm"
+                    onChange={(e) => setFilterByStatus(e.target.value)}
+                  >
+                    {statusArray.map((status) => (
+                      <option key={status}>{status}</option>
+                    ))}
+                  </select>
+                </div>
+                {filteredOrders.map((order) => {
+                  return (
+                    <div
+                      key={order.id}
+                      className="bg-white shadow-lg rounded-lg p-6 mb-6 transition-all"
+                    >
+                      <div className="flex justify-between items-start flex-wrap gap-4">
+                        <div>
+                          <p>
+                            <strong>Order ID:</strong> {order.id}
+                          </p>
+                          <p>
+                            <strong>Status:</strong> {order.order_status}
+                          </p>
+                          <p>
+                            <strong>Placed At:</strong>{" "}
+                            {new Date(order.created_at).toLocaleString()}
+                          </p>
+                          <p>
+                            <strong>Total Amount:</strong>৳ {order.total_price}
+                          </p>
+                        </div>
+                        <div>
+                          <select
+                            value={
+                              selectedStatus[order.id] || order.order_status
+                            }
+                            onChange={(e) =>
+                              handleStatusChange(order.id, e.target.value)
+                            }
+                            className="border p-2 rounded mb-2"
+                          >
+                            {statusArray.map((status) => (
+                              <option key={status} value={status}>
+                                {status}
+                              </option>
+                            ))}
+                          </select>
+                          <button
+                            onClick={() => {
+                              setDeleteConfirm({ open: true, id: order.id });
+                            }}
+                            className="ml-3 bg-red-500 hover:bg-red-600 text-white rounded px-3 py-1"
+                          >
+                            Delete
+                          </button>
+                        </div>
+                      </div>
+
+                      <div className="mt-4">
+                        <h4 className="font-semibold text-lg mb-1">
+                          Shipping Info
+                        </h4>
+                        <p>
+                          <strong>Name:</strong>{" "}
+                          {order.shipping_info?.full_name}
+                        </p>
+                        <p>
+                          <strong>Phone:</strong> {order.shipping_info?.phone}
+                        </p>
+                        <p>
+                          <strong>Address:</strong>{" "}
+                          {order.shipping_info?.address},{" "}
+                          {order.shipping_info?.city},
+                          {order.shipping_info?.state},
+                          {order.shipping_info?.pincode}
+                        </p>
+                      </div>
+
+                      <div className="mt-4">
+                        <h4 className="font-semibold text-lg mb-2">
+                          Ordered Items
+                        </h4>
+                        {Array.isArray(order.order_items) &&
+                          order.order_items.map((item) => {
+                            return (
+                              <div
+                                key={item.order_item_id}
+                                className="flex items-center gap-4 mb-2 border-b pb-2"
+                              >
+                                {item.image && (
+                                  <img
+                                    src={item.image}
+                                    alt={item.title}
+                                    className="w-16 h-16 object-cover cursor-pointer"
+                                    onClick={() => setPreviewImage(item.image)} // Note: This line seems incomplete in the image, likely meant to set the preview image
+                                  />
+                                )}
+                                <div>
+                                  <p className="font-semibold">{item.title}</p>
+                                  <p>
+                                    <strong>Qty:</strong>
+                                    {item.quantity}|<strong>Price:</strong> ৳
+                                    {item.price}|<strong>Total Price:</strong> ৳
+                                    {item.quantity * item.price}
+                                  </p>
+                                </div>
+                              </div>
+                            );
+                          })}
+                      </div>
+                    </div>
+                  );
+                })}
+              </>
+            )}
