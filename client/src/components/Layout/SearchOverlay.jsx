d508835a4125946da455671c30377f17da7d567a
Kamrul Hasan <116941825+kamrul35728@users.noreply.github.com>
2025-12-01 17:10:49 +0600

    Add SearchOverlay component with state management
    add some states

--- client/src/components/Layout/SearchOverlay.jsx
+++ client/src/components/Layout/SearchOverlay.jsx
@@ -3,3 +3,10 @@ import { X, Search } from "lucide-react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { toggleSearchBar } from "../../store/slices/popupSlice";
+const SearchOverlay = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+};
+
+export default SearchOverlay;
