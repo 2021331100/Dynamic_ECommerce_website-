160e610d06c4867b20b154c0525350cf9fa3efdb
2021331100 <138570295+2021331100@users.noreply.github.com>
2025-12-01 07:51:04 -0800

    for order state specification Update Orders.jsx

--- dashboard/src/components/Orders.jsx
+++ dashboard/src/components/Orders.jsx
@@ -1,1 +1,17 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Header from "./Header";
+import {
+  updateOrderStatus,
+  deleteOrder,
+  fetchAllOrders,
+} from "../store/slices/orderSlice";
 
+const Orders = () => {
+  const statusArray = [
+    "All",
+    "Processing",
+    "Shipped",
+    "Delivered",
+    "Cancelled",
+  ];
