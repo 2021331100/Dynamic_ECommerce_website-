e55f5988ff75bcdb10e76dbad58d6bdd86fe80f8
Kamrul Hasan <116941825+kamrul35728@users.noreply.github.com>
2025-12-01 16:30:04 +0600

    Add imports for ProfilePanel component
    import all things

--- client/src/components/Layout/ProfilePanel.jsx
+++ client/src/components/Layout/ProfilePanel.jsx
@@ -1,1 +1,6 @@
-
+import { useEffect, useState } from "react";
+import { X, LogOut, Upload, Eye, EyeOff } from "lucide-react";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import {logout,updatePassword,updateProfile,} from "../../store/slices/authSlice";
+import { toggleAuthPopup } from "../../store/slices/popupSlice";
