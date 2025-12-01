d97fa0b647b5205da8eb93906953c13e56e188e2
Kamrul Hasan <116941825+kamrul35728@users.noreply.github.com>
2025-12-01 16:33:26 +0600

    Initialize ProfilePanel component state

--- client/src/components/Layout/ProfilePanel.jsx
+++ client/src/components/Layout/ProfilePanel.jsx
@@ -4,3 +4,19 @@ import { useDispatch, useSelector } from "react-redux";
 import { toast } from "react-toastify";
 import {logout,updatePassword,updateProfile,} from "../../store/slices/authSlice";
 import { toggleAuthPopup } from "../../store/slices/popupSlice";
+
+
+const ProfilePanel = () => {
+  const dispatch = useDispatch();
+  const { isAuthPopupOpen } = useSelector((state) => state.popup);
+  const { authUser, isUpdatingProfile, isUpdatingPassword } = useSelector(
+    (state) => state.auth
+  );
+
+  const [name, setName] = useState(authUser?.name || "");
+  const [email, setEmail] = useState(authUser?.email || "");
+  const [avatar, setAvatar] = useState(null);
+  
+  
+};
+export default ProfilePanel;
