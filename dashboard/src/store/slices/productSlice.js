b2d7d1721a890d99db7f481652f2b85ebb6b8175
2021331100 <138570295+2021331100@users.noreply.github.com>
2025-12-03 10:38:51 -0800

    implemented state for all fetches state for productslice.js

--- dashboard/src/store/slices/productSlice.js
+++ dashboard/src/store/slices/productSlice.js
@@ -1,1 +1,68 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { axiosInstance } from "../../lib/axios";
+import { toast } from "react-toastify";
+import {
+  toggleCreateProductModal,
+  toggleUpdateProductModal,
+}from "./extraSlice";
+const productSlice = createSlice({
+  name: "product",
+  initialState: {
+    loading: false,
+    fetchingProducts: false,
+    products: [],
+    totalProducts: 0,
+  },
+  reducers: {
+    createProductRequest(state) {
+      state.loading = true;
+    },
+    createProductSuccess(state, action) {
+      state.loading = false;
+      state.products = [action.payload, ...state.products];
+    },
+    createProductFailed(state) {
+      state.loading = false;
+    },
+
+    getAllProductsRequest(state) {
+      state.fetchingProducts = true;
+    },
+    getAllProductsSuccess(state, action) {
+      state.fetchingProducts = false;
+      state.products = action.payload.products;
+      state.totalProducts = action.payload.totalProducts;
+    },
+    getAllProductsFailed(state) {
+      state.fetchingProducts = false;
+    },
+
+    updateProductRequest(state) {
+      state.loading = true;
+    },
+    updateProductSuccess(state, action) {
+      state.loading = false;
+      state.products = state.products.map((product) =>
+        product.id === action.payload.id ? action.payload : product
+      );
+    },
+    updateProductFailed(state) {
+      state.loading = false;
+    },
+
+    deleteProductRequest(state) {
+      state.loading = true;
+    },
+    deleteProductSuccess(state, action) {
+      state.loading = false;
+      state.products = state.products.filter(
+        (product) => product.id !== action.payload
+      );
+      state.totalProducts = Math.max(0, state.totalProducts - 1);
+    },
+    deleteProductFailed(state) {
+      state.loading = false;
+    },
+  },
+});
 
